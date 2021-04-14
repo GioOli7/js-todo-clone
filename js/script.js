74c2763d2e6a87ba185728e7979f174a85f9a8c2
GioOli7 <gio07live@gmail.com>
2021-04-14 16:24:04 +0200

    todo list popolation

--- js/script.js
+++ js/script.js
@@ -1,6 +1,52 @@
+/*
+Rifare la todo list come vista insieme a lezione:
+1 - popolando gli elementi della lista con JavaScript
+2 - inserendo un nuovo todo con un input testuale e gli eventi da tastiera
+3 - Rimozione todo con click su icona
+4 - Cliccando sul testo compare uno sbarramento a indicarne il completamento
+*/
+
+
+
 $(document).ready(function() {
     
-    
+    // refs
+    var template = $('#template .app_todos-item')
+    var todos = $('.app_todos')
+    var newInput = $('#input');
+    var listItem = $('.app_todos-item');
+    var deleteItem = $('.app_todos-item i.delete-icon');
+
+    var todoItems = [
+        {
+            text: "Comprare la pasta",
+            done: false,
+        },
+        {
+            text: "Pagare le bollette",
+            done: false,
+        },
+        {
+            text: "Telefonare dentista",
+            done: false,
+        },
+        {
+            text: "Telefonare commercialista",
+            done: false,
+        },
+    ];
+
+    // 1 - popolazione lista
+    for (var i = 0; i <todoItems.length; i++) {
+        var todoItem = todoItems[i];
+        // mi copio il template
+        var cloneItem = template.clone();
+        // inserisco il testo dell oggetto
+        cloneItem.find('.text').text(todoItem.text);
+        // aggiungo il tutto alla lista
+        todos.append(cloneItem);
+    }
+
 
 
 
